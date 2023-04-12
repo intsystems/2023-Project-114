3c68f2ef4ed41828f3a67e7c323e899cffd09135
Alexander <bogdanov.ai@phystech.edu>
2023-04-13 00:58:46 +0300

    all_update_3

--- code/README.md
+++ code/README.md
@@ -3,5 +3,5 @@
 ## Run train
 
 ```bash
-python3 train.py -с configs/example.yaml
+python3 train.py "name_of_experiment" number_of_file start_index
 ```
--- code/train.py
+++ code/train.py
@@ -1,23 +1,20 @@
-import pickle
-from datetime import datetime
-from functools import partial
-from pathlib import Path
 import jax
+import jax.numpy as jnp
 from jax.example_libraries import stax
 from jax.example_libraries import optimizers
-import jax.numpy as jnp
-import sys
-from dataset.make_data import equation_of_motion, rk4_step, solve_lagrangian
-from dataset.plot import normalize_dp
-from visualization import plot_loss
+from jax.experimental.ode import odeint
+from functools import partial
+from pathlib import Path
+import matplotlib.pyplot as plt
 import numpy as np
-
+import pickle
+import sys
 
 TRAIN_DATASET_PATH = Path("trajectories")
 TEST_DATASET_PATH = Path("trajectories")
 LOG_DIR = Path("logs")
 
-# build a neural network model
+# neural network model
 init_random_params, nn_forward_fn = stax.serial(
         stax.Dense(128),
         stax.Softplus,
@@ -26,30 +23,59 @@ init_random_params, nn_forward_fn = stax.serial(
         stax.Dense(1),
     )
 
-# replace the lagrangian with a parameteric model
+def equation_of_motion(lagrangian, state, t=None):
+    """
+    getting acceleration
+    """
+    q, q_t = jnp.split(state, 2)
+    q_tt = (jnp.linalg.pinv(jax.hessian(lagrangian, 1)(q, q_t))
+            @ (jax.grad(lagrangian, 0)(q, q_t)
+                - jax.jacobian(jax.jacobian(lagrangian, 1), 0)(q, q_t) @ q_t))
+    return jnp.concatenate([q_t, q_tt])
+
+def normalize_dp(state):
+    """
+    wrap generalized coordinates to [-pi, pi]
+    """
+    return jnp.concatenate([(state[:2] + np.pi) % (2 * np.pi) - np.pi, state[2:]])
+
 def learned_lagrangian(params):
+    """
+    replace the lagrangian with a parameteric model
+    """
     def lagrangian(q, q_t):
         assert q.shape == (3,)
         state = normalize_dp(jnp.concatenate([q, q_t]))
         return jnp.squeeze(nn_forward_fn(params, state), axis=-1)
     return lagrangian
 
-# define the loss of the model (MSE between predicted q, \dot q and targets)
 @jax.jit
-def loss(params, batch, time_step=None):
+def loss(params, batch):
+    """
+    define the loss of the model
+    """
     state, targets = batch
-    if time_step is not None:
-        f = partial(equation_of_motion, learned_lagrangian(params))
-        preds = jax.vmap(partial(rk4_step, f, t=0.0, h=time_step))(state)
-    else:
-        preds = jax.vmap(partial(equation_of_motion, learned_lagrangian(params)))(state)
+    preds = jax.vmap(partial(equation_of_motion, learned_lagrangian(params)))(state)
     return jnp.mean((preds - targets) ** 2)
 
-def predict_lnn(params, x, t):
-    return jax.device_get(solve_lagrangian(learned_lagrangian(params), x, t=t))
+def plot_loss(train_losses, test_losses, log_dir):
+    """
+    plot of loss
+    """
+    plt.figure(figsize=(8, 3.5), dpi=120)
+    plt.plot(train_losses, label='Train loss')
+    plt.plot(test_losses, label='Test loss')
+    plt.title('Losses over training')
+    plt.xlabel("Train step")
+    plt.ylabel("Mean squared error")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(Path("logs", "loss.png"), dpi=150)
 
 def train(init_random_params, x_train, xt_train, x_test, xt_test, log_dir=None, name=""):
-
+    """
+    model training
+    """
     rng = jax.random.PRNGKey(0)
     _, init_params = init_random_params(rng, (-1, 6))
 
@@ -71,8 +97,7 @@ def train(init_random_params, x_train, xt_train, x_test, xt_test, log_dir=None, 
     @jax.jit
     def update_derivative(i, opt_state, batch):
         params = get_params(opt_state)
-        return opt_update(i, jax.grad(loss)(params, batch, None), opt_state)
-
+        return opt_update(i, jax.grad(loss)(params, batch), opt_state)
 
     for iteration in range(batch_size * num_batches + 1):
         if iteration % batch_size == 0:
@@ -93,17 +118,15 @@ def train(init_random_params, x_train, xt_train, x_test, xt_test, log_dir=None, 
     params_numpy = np.hstack(params_arr)
     
     if log_dir is not None:
-        with open(Path("logs", f"{name}"), 'wb') as handle:
+        with open(Path(LOG_DIR, f"{name}"), 'wb') as handle:
             pickle.dump(params, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        plot_loss(train_losses, test_losses, model_name='lnn', log_dir=log_dir)
+        plot_loss(train_losses, test_losses, log_dir=LOG_DIR)
 
 def main():
 
     experiment_name = str(sys.argv[1])
-
     n = int(sys.argv[2])
-    
     start_idx = int(sys.argv[3])
     
     for i in range(start_idx, start_idx + n):
